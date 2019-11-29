f9708bb6d3e9d95e64f3a3224e458a1217e68b49
barabasi_zsolt <nevis300@gmail.com>
2019-11-29 13:28:00 +0200

    new project created

---
+++ algorythmics_test_1/algorythmics_test_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algorythmics_test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
