dc8684c3f5c823767d5137a1cc59dcea6a6e1a8d
ZsoltVagasi <zsoltvagasi6@gmail.com>
2019-12-05 14:12:38 +0200

    Regisztracio s a tobbi

--- connection_C_and_SQL/Program.cs
+++ connection_C_and_SQL/Program.cs
@@ -7,26 +7,66 @@ namespace TestApplication
 {
     class Program
     {
-        static string connectionString = "SERVER = .\\SQLEXPRESS; DATABASE = Customers; USER ID = zsolt; PASSWORD = 12345678; Trusted_Connection=True;";
-        static void Main(string[] args)
+		
+		static string connectionString = "SERVER = DESKTOP-9A2J9E7\\SQLEXPRESS; DATABASE = MatematikApp; USER ID = Vagasi; PASSWORD = 123; Trusted_Connection=True;";
+		SqlConnection sqlcon = new SqlConnection(connectionString);
+		static void Main(string[] args)
         {
 
             string my_query;
+			string query1, query2;
+			// Console.WriteLine("Olvasson be SQL utasitasokat * vegjelig: ", '\n');
+			//Console.WriteLine("Olvasson be egy SQL utasitast: ", '\n');
+			Console.WriteLine("Valassza ki az utasitast \n Login \n Register \n User \n Insert");
+				string Command;
+				Command = Console.ReadLine();
+				switch(Command)
+				{
+					case "Login":
+					string query1, query2;
+					Console.WriteLine("Felhasznalonev");
+							query1 = Console.ReadLine();
+						Console.WriteLine("\n Jelszo");
+							query2 = Console.ReadLine();
 
-            Console.WriteLine("Olvasson be SQL utasitasokat * vegjelig: ", '\n');
+					SqlCommand cmd = new SqlCommand(my_query, sqlcon);
+					break;
+				case "Register":
+					
+					Console.WriteLine("Felhasznalonev");
+					string query1, query2, query3, query4, query5, query6, query7, query8;
+					Console.WriteLine("\n Felhasznalo: ");
+					query1 = Console.ReadLine();
+					Console.WriteLine("\n Jelszo: ");
+					query2 = Console.ReadLine();
+					Console.WriteLine("\n Jelszo Megerosites: ");
+					query3 = Console.ReadLine();
+					Console.WriteLine("\n Szerep: ");
+					query4 = Console.ReadLine();
+					Console.WriteLine("\n Szak: ");
+					query5 = Console.ReadLine();
+					Console.WriteLine("\n Evfolyam: ");
+					query6 = Console.ReadLine();
+					Console.WriteLine("\n T.Szam: ");
+					query7 = Console.ReadLine();
+					Console.WriteLine("\n Tantargy: ");
+					query8 = Console.ReadLine();
+					if (query2 = query3)
+						my_query = "Insert Into Regisztracio (Nev,Jelszo,Jelszo_Megerosites,Role,Szak,Evfolyam,TantargySzam,Tantargy) Values (query1,query2,query3,query4,query5,query6,query7,query8)";
+					else
+						Console.WriteLine("A jelszavak nem egyeznek meg");
+					SqlCommand cmd = new SqlCommand(my_query, sqlcon);
+					break;
 
-           for(int j = 0; ; ++j)
-            {
-                SqlConnection sqlcon = new SqlConnection(connectionString);
 
-                Console.WriteLine("Olvasson be egy SQL utasitast: ", '\n');
+			}
 
                 my_query = Console.ReadLine();
 
-                if (my_query.Equals("*")) {
+                /*if (my_query.Equals("*")) {
 
                     break;
-                }
+                }*/
 
                 SqlCommand cmd = new SqlCommand(my_query, sqlcon);
 
@@ -63,7 +103,7 @@ namespace TestApplication
 
                 }
 
-            }
+            
         }
     }
 }
